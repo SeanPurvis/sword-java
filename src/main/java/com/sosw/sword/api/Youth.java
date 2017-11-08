a7637a946039d8acefdbf2162a68dc1fd0cb3b37
Unknown <Sean.Purvis@usm.edu>
2017-11-08 15:23:39 -0600

    Add Youth Table Representation

---
+++ src/main/java/com/sosw/sword/api/Youth.java
@@ -0,0 +1,91 @@
+package com.sosw.sword.api;
+
+import java.sql.Date;
+
+public class Youth {
+	private int id;
+	private String ssn;
+	private String race;
+	private String gender;
+	private Date dob;
+	private String birthcity;
+	private String birthstate;
+	private String isadult;
+	private String immediate_medical;
+	
+	public Youth() {
+		//This is needed for Jackson deserialization
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+	public String getRace() {
+		return race;
+	}
+
+	public void setRace(String race) {
+		this.race = race;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public String getBirthcity() {
+		return birthcity;
+	}
+
+	public void setBirthcity(String birthcity) {
+		this.birthcity = birthcity;
+	}
+
+	public String getBirthstate() {
+		return birthstate;
+	}
+
+	public void setBirthstate(String birthstate) {
+		this.birthstate = birthstate;
+	}
+
+	public String getIsadult() {
+		return isadult;
+	}
+
+	public void setIsadult(String isadult) {
+		this.isadult = isadult;
+	}
+
+	public String getImmediate_medical() {
+		return immediate_medical;
+	}
+
+	public void setImmediate_medical(String immediate_medical) {
+		this.immediate_medical = immediate_medical;
+	}
+} 
