b0a2365018ea29ba15f3fce99f3b6c5bc36dbe2e
Unknown <Sean.Purvis@usm.edu>
2017-11-08 15:33:52 -0600

    Fix RoleCaps Representation

--- src/main/java/com/sosw/sword/api/RoleCaps.java
+++ src/main/java/com/sosw/sword/api/RoleCaps.java
@@ -7,4 +7,22 @@ public class RoleCaps {
 	public RoleCaps() {
 		//Needed for Jackson deserialization
 	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getCap() {
+		return cap;
+	}
+
+	public void setCap(String cap) {
+		this.cap = cap;
+	}
+	
+	
 }
