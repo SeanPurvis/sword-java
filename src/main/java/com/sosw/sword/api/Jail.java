ed903800fb098d3d0d9e25b5ce3ead59a6c96ced
Unknown <Sean.Purvis@usm.edu>
2017-11-08 16:06:41 -0600

    Add Jail Representation

---
+++ src/main/java/com/sosw/sword/api/Jail.java
@@ -0,0 +1,46 @@
+package com.sosw.sword.api;
+
+public class Jail {
+	private int id;
+	private String name;
+	private String unit;
+	private String cells;
+	
+	public Jail() {
+		//Needed for Jackson deserialization
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+	public String getCells() {
+		return cells;
+	}
+
+	public void setCells(String cells) {
+		this.cells = cells;
+	}
+	
+	
+}
