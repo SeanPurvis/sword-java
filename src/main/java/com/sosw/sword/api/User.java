9a8817d25f887756bfae50ede94defe09575e528
Unknown <Sean.Purvis@usm.edu>
2017-11-08 15:23:26 -0600

    Add User Table Representation

---
+++ src/main/java/com/sosw/sword/api/User.java
@@ -0,0 +1,80 @@
+package com.sosw.sword.api;
+
+public class User {
+	private int id;
+	private String username;
+	private String name;
+	private String phone;
+	private String password;
+	private String employer;
+	private String role;
+	private String email;
+	
+	public User() {
+		//Needed for Jackson Deserialization
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmployer() {
+		return employer;
+	}
+
+	public void setEmployer(String employer) {
+		this.employer = employer;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
