1096c7f5b46568decf8bee42ac548cc0f170c4ea
Unknown <Sean.Purvis@usm.edu>
2017-11-08 16:21:42 -0600

    Add ChargeList Representation

---
+++ src/main/java/com/sosw/sword/api/ChargeList.java
@@ -0,0 +1,13 @@
+package com.sosw.sword.api;
+
+public class ChargeList {
+	private int id;
+	private String category;
+	private String description;
+	private String statute;
+	private String type; //This field needs to be called class in the static_charge_list table
+	private int risk_assessment_value;
+	private String petition_name;
+	private String petition_child_declaration;
+	private String petition_crime_desc_template;
+}
