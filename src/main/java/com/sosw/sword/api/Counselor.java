400ed80d35d02176a30596f8cec619ccd739a1bc
Unknown <Sean.Purvis@usm.edu>
2017-11-09 16:40:33 -0600

    Add Counselor Representation

---
+++ src/main/java/com/sosw/sword/api/Counselor.java
@@ -0,0 +1,46 @@
+package com.sosw.sword.api;
+
+public class Counselor {
+	private int id;
+	private String name;
+	private String email;
+	private int status;
+	
+	public Counselor() {
+		//Needed for Jackson deserialization
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	
+	
+}
