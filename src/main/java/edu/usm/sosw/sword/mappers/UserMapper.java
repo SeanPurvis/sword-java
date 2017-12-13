3e2ece3e304c3789a982401eee1a86777e04f6ac
Unknown <Sean.Purvis@usm.edu>
2017-12-13 15:00:51 -0600

    Improve UserMapper documentation

--- src/main/java/edu/usm/sosw/sword/mappers/UserMapper.java
+++ src/main/java/edu/usm/sosw/sword/mappers/UserMapper.java
@@ -8,6 +8,14 @@ import org.skife.jdbi.v2.tweak.ResultSetMapper;
 
 import edu.usm.sosw.sword.api.User;
 
+/**
+ * 
+ * @author Sean T. Purvis
+ * @version 0.0.1
+ *
+ * This UserMapper provides the functionality to map SQL query results to a User object. 
+ * 
+ */
 public class UserMapper implements ResultSetMapper<User> {
 	public User map(int index, ResultSet r, StatementContext ctx) throws SQLException {
 		return new User(r.getInt("id"), r.getString("username"), r.getString("name"), r.getString("phone"),
