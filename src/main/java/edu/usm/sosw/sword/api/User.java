d5e211bb62b748c7ee9959488a5cee63d0fb227c
Unknown <Sean.Purvis@usm.edu>
2017-12-13 14:28:58 -0600

    Add Constructor and AutoGenerated helper functions to User POJO

--- src/main/java/edu/usm/sosw/sword/api/User.java
+++ src/main/java/edu/usm/sosw/sword/api/User.java
@@ -16,6 +16,19 @@ public class User {
 		//Needed for Jackson Deserialization
 	}
 
+	public User(int id, String username, String name, String phone, String password, String employer, String role,
+			String email) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.name = name;
+		this.phone = phone;
+		this.password = password;
+		this.employer = employer;
+		this.role = role;
+		this.email = email;
+	}
+
     @JsonProperty
 	public int getId() {
 		return id;
@@ -95,4 +108,77 @@ public class User {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((employer == null) ? 0 : employer.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (employer == null) {
+			if (other.employer != null)
+				return false;
+		} else if (!employer.equals(other.employer))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (phone == null) {
+			if (other.phone != null)
+				return false;
+		} else if (!phone.equals(other.phone))
+			return false;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", name=" + name + ", phone=" + phone + ", password="
+				+ password + ", employer=" + employer + ", role=" + role + ", email=" + email + "]";
+	}
 }
+
+
+
