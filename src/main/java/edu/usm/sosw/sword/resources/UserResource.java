5223cc3db568a4aff20e1efe9d0b296eb694bdfd
Unknown <Sean.Purvis@usm.edu>
2017-12-14 14:31:32 -0600

    Add password hashing to new User insert

--- src/main/java/edu/usm/sosw/sword/resources/UserResource.java
+++ src/main/java/edu/usm/sosw/sword/resources/UserResource.java
@@ -3,7 +3,6 @@ package edu.usm.sosw.sword.resources;
 import java.util.List;
 
 import javax.validation.Valid;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -12,6 +11,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.mindrot.jbcrypt.BCrypt;
 
 import edu.usm.sosw.sword.api.User;
 import edu.usm.sosw.sword.db.UserDAO;
@@ -51,6 +52,8 @@ public class UserResource {
 	
 	@POST
 	public int add(@Valid User user) {
+		// Pull the user password, pass it to the hashing function, and then set the hash as the object's password.
+		user.setPassword(hashPassword(user.getPassword()));
 		return userDAO.insert(user);
 	}
 	
@@ -65,4 +68,40 @@ public class UserResource {
 	public void delete(@PathParam("id") Integer id) {
 		userDAO.deleteById(id);
 	}
+	
+    
+    /**
+     * This method is used to generate a string representing an account password
+     * suitable for database storage. It will be OpenBSD style crypt formatted
+     * hash string of length=60
+     * The bcrypt workload is specified in the final variable below.
+     * @param password_plaintext the user's plaintext password provided when
+     * creating the new account or logging in.
+     * @return String - a string of length 60 that is the bcrypt hashed password in crypt(3) format.
+     */
+    public static String hashPassword(String password_plaintext) {
+    	final int workload = 16;
+    	String salt = BCrypt.gensalt(workload);
+    	String hashed_password = BCrypt.hashpw(password_plaintext, salt);
+    	
+    	return(hashed_password);
+    }
+    
+    /**
+     * This method will verify a computed hash from a plaintext with that of the hash stored in the database. 
+     * @param password_plaintext The user's plaintext password as provided during the login request.
+     * @param stored_hash The account's stored password from the database. 
+     * @return boolean - true if the password matches the password of the stored hash, false otherwise. 
+     */
+    public static boolean checkPassword(String password_plaintext, String stored_hash) {
+    	boolean password_verified = false;
+    	
+    	if(null == stored_hash || !stored_hash.startsWith("$2a$"))
+    		throw new java.lang.IllegalArgumentException("Invalid hash provided for comparison");
+    	
+    	password_verified = BCrypt.checkpw(password_plaintext, stored_hash);
+    	
+    	return(password_verified);
+    }
+    
 }
